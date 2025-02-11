39fdc352c04e9ba6b4fc1bd9e049a9fd70100f9f
kefale-Dev24 <kefaleseifu@gmail.com>
2025-02-11 10:40:44 +0300

    first-commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Kefe this is a Great Success !!!</h1>
+    </>
+  )
+}
+
+export default App
