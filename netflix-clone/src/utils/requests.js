003d47c8487573eb5b783f081ef4f5a92e860aef
kefale-Dev24 <kefaleseifu@gmail.com>
2025-02-16 19:22:29 +0300

    base

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -4,7 +4,7 @@
 // New API KEY
 // const API_KEY = process.env.REACT_APP_MOVIE_DB_api_key;
 const API_KEY = import.meta.env.VITE_MOVIE_DB_api_key;
-                                    
+
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
